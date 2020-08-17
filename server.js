2b862b0c236c028031793bb66e7d34fa76a2c713
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-17 16:50:40 -0700

    cleaned server file

--- server.js
+++ server.js
@@ -4,39 +4,41 @@ const bcrypt = require("bcrypt");
 
 const app = express();
 
-
 const PORT = process.env.PORT || 8080;
 require("dotenv").config();
 const session = require("express-session");
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const db = require("./models");
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:3000"], // ["https://betutor2020.herokuapp.com/"],
-  credentials:true
-})); 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // ["https://betutor2020.herokuapp.com/"],
+    credentials: true,
+  })
+);
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  store: new SequelizeStore({
-    db: db.sequelize
-  }),
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 3600000
-  }
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: new SequelizeStore({
+      db: db.sequelize,
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600000,
+    },
+  })
+);
 
-app.use(express.static("public"))
-
-
+app.use(express.static("public"));
 
 const logInRoute = require("./controllers/loginController.js");
 const studentRoute = require("./controllers/studentController.js");
@@ -46,7 +48,6 @@ const teacherRoute = require("./controllers/teacherController.js");
 const filterRoute = require("./controllers/filterController.js");
 const matchingRoute = require("./controllers/matchingController.js");
 
-
 app.use(logInRoute);
 app.use(studentRoute);
 app.use(reviewRoute);
@@ -55,22 +56,15 @@ app.use(teacherRoute);
 app.use(filterRoute);
 app.use(matchingRoute);
 
-
-// app.use("/route", teacherRoute);
-
-//SET ALL ROUTES// let signupRoute = require("./controllers/signupController.js");
-// app.use(signupRoute);
-
-//app.use(/*/route*/, teacherRoute);
-
-
-
-db.sequelize.sync({
-  force: false
-}).then(function () {
-  app.listen(PORT, function () {
-    console.log('App listening on PORT ' + PORT);
+db.sequelize
+  .sync({
+    force: false,
+  })
+  .then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
+  })
+  .catch((err) => {
+    throw err;
   });
-}).catch(err => {
-  throw err;
-});
