2f2bceee351542d2b3c9cefe179e37e5349fc838
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-19 15:34:16 -0700

    session logs

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -40,6 +40,7 @@ router.post("/login", function (req, res) {
     });
 });
 router.get("/readsessions", (req, res) => {
+    console.log("*********************************************************", req.session);
     res.json(req.session)
 })
 router.get("/logout",(req,res)=>{
