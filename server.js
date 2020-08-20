96bc1b783e0ae4e01dbb66189dd8109ef687b606
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-20 13:50:21 -0700

    AS it is, it works in development

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    // origin: ["http://localhost:3000"],
-    origin: ["https://you-tutor.herokuapp.com","http://localhost:3000"],
+    origin: ["http://localhost:3000"],
+    // origin: ["https://you-tutor.herokuapp.com"],
     credentials: true,
   })
 );
@@ -35,8 +35,6 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 3600000,
-      samesite:'strict',
-      secure:true
     },
   })
 );
