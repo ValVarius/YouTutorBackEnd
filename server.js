19fbd716652cf83c9256158b90b380512a15f8fd
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-19 18:50:40 -0700

    samesite attribue for cookie

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    // origin: ["http://localhost:3000"],
-    origin: ["https://you-tutor.herokuapp.com","http://localhost:3000"],
+    origin: ["http://localhost:3000"],
+    // origin: ["https://you-tutor.herokuapp.com","http://localhost:3000"],
     credentials: true,
   })
 );
