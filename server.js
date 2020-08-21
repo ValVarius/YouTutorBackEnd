43b678fda745a6e51d079f80dfb5bad059f856ad
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-20 17:07:40 -0700

    secure set false

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ app.use(
     // origin: ["http://localhost:3000"],
     origin: ["https://you-tutor.herokuapp.com"],
     credentials: true,
-    samesite:'strict',
-    secure:true
+    secure:false
   })
 );
 
