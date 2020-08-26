b753aeefd3481ba51c537df2579a6d9c9c526648
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-08-25 21:49:47 -0700

    new cors for new dns

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(express.json());
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
-    // origin: ["http://www.you-tutor.com/","https://you-tutor.herokuapp.com","http://localhost:3000"],
-    origin: ["http://www.you-tutor.com/"],
+    // origin: ["http://www.you-tutor.com","https://you-tutor.herokuapp.com","http://localhost:3000"],
+    origin: ["http://www.you-tutor.com"],
     credentials: true,
   })
 );
